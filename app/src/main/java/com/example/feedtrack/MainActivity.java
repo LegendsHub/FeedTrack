f3336aa06b39c11eeddc477a0319ebc46af292aa
Saurabh <saurabhsalunke91@gmail.com>
2023-04-04 12:17:06 +0530

    Login authenticated

--- app/src/main/java/com/example/feedtrack/MainActivity.java
+++ app/src/main/java/com/example/feedtrack/MainActivity.java
@@ -6,21 +6,30 @@ import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.feedtrack.databinding.ActivityMainBinding;
 import com.example.feedtrack.fragments.sem4;
 import com.example.feedtrack.fragments.sem5;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
    ActivityMainBinding binding;
-
+    FirebaseAuth auth;
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
         binding.semesterSpinner.setOnItemSelectedListener(this);
         // Make API call to retrieve user's name and PRN number
         getUserInfo();
