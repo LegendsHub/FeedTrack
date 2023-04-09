ebcfe821a822d3ce1fff593173466501a9cc9449
Saurabh <saurabhsalunke91@gmail.com>
2023-04-09 17:24:21 +0530

    Feat:user authentication done

--- app/src/main/java/com/example/feedtrack/fragments/student.java
+++ app/src/main/java/com/example/feedtrack/fragments/student.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class student extends Fragment {
@@ -48,23 +51,51 @@ FragmentStudentBinding binding;
         binding.signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pd.show();
-                mAuth.createUserWithEmailAndPassword
-                        (binding.signupEmail.getText().toString(), binding.signupPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            pd.dismiss();
-                            Users user = new Users(binding.uname.getText().toString(), binding.signupPassword.getText().toString(), binding.signupEmail.getText().toString());
-                            String id = task.getResult().getUser().getUid();
-                            database.getReference().child("Students").child(binding.uname.getText().toString()).setValue(user);
-                            startActivity(new Intent(getContext(), login.class));
-                            Toast.makeText(getContext(), "Account Created Successfully...", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                String uname=binding.uname.getText().toString().trim();
+                String email=binding.signupEmail.getText().toString().trim();
+                String password=binding.signupPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(uname)){
+                    Toast.makeText(getContext(),"username field should not be empty", Toast.LENGTH_SHORT).show();
+                    binding.uname.setError("Username is required");
+                    binding.uname.requestFocus();
+                }
+                else if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getContext(),"Email field should not be empty", Toast.LENGTH_SHORT).show();
+                    binding.signupEmail.setError("Email is required");
+                    binding.signupEmail.requestFocus();
+                }
+                else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getContext(),"password field should not be empty", Toast.LENGTH_SHORT).show();
+                    binding.signupPassword.setError("Password is required");
+                    binding.signupPassword.requestFocus();
+                }
+                else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    binding.signupEmail.setError("enter valid email");
+                    binding.signupEmail.requestFocus();
+                }
+                else {
+                    pd.show();
+                    mAuth.createUserWithEmailAndPassword
+                            (binding.signupEmail.getText().toString(), binding.signupPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                pd.dismiss();
+                                Users user = new Users(binding.uname.getText().toString(), binding.signupPassword.getText().toString(), binding.signupEmail.getText().toString());
+                                String id = task.getResult().getUser().getUid();
+                                database.getReference().child("Students").child(binding.uname.getText().toString()).setValue(user);
+                                Intent intent=new Intent(getContext(), login.class);
+//                                intent.putExtra("d","student");
+//                                intent.putExtra("u",binding.uname.getText().toString().trim());
+                                startActivity(intent);
+                                Toast.makeText(getContext(), "Account Created Successfully...", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
         return binding.getRoot();
